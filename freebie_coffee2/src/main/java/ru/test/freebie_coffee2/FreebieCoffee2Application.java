491f9855fac8ea2d3e04ad70103ea1396236b62d
Nikita <nikitamasslennikov@mail.ru>
2023-04-26 20:24:18 +0300

    Initial commit

---
+++ freebie_coffee2/src/main/java/ru/test/freebie_coffee2/FreebieCoffee2Application.java
@@ -0,0 +1,13 @@
+package ru.test.freebie_coffee2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreebieCoffee2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreebieCoffee2Application.class, args);
+	}
+
+}
---
+++ freebie_coffee2/src/test/java/ru/test/freebie_coffee2/FreebieCoffee2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.test.freebie_coffee2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreebieCoffee2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
