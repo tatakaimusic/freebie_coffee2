3a943bc5d3adcbc9ef904584b33c4b2eb334de08
Nikita <nikitamasslennikov@mail.ru>
2023-04-27 07:08:39 +0300

    auth implementation without AuthProviderImpl.java

--- freebie_coffee2/src/main/java/ru/test/freebie_coffee2/config/SecurityConfig.java
+++ freebie_coffee2/src/main/java/ru/test/freebie_coffee2/config/SecurityConfig.java
@@ -1,21 +1,29 @@
 package ru.test.freebie_coffee2.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import ru.test.freebie_coffee2.security.AuthProviderImpl;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import ru.test.freebie_coffee2.services.PersonDetailsService;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    private final AuthProviderImpl authProvider;
+    private final PersonDetailsService personDetailsService;
 
     @Autowired
-    public SecurityConfig(AuthProviderImpl authProvider) {
-        this.authProvider = authProvider;
+    public SecurityConfig(PersonDetailsService personDetailsService) {
+        this.personDetailsService = personDetailsService;
     }
 
-    protected void configure(AuthenticationManagerBuilder auth) {
-        auth.authenticationProvider(authProvider);
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(personDetailsService);
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
     }
 }
--- freebie_coffee2/src/main/java/ru/test/freebie_coffee2/contollers/RootController.java
+++ freebie_coffee2/src/main/java/ru/test/freebie_coffee2/contollers/RootController.java
@@ -1,10 +1,14 @@
 package ru.test.freebie_coffee2.contollers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ru.test.freebie_coffee2.models.Person;
+import ru.test.freebie_coffee2.security.PersonDetails;
 import ru.test.freebie_coffee2.services.ProductService;
 
 @Controller
@@ -21,6 +25,13 @@ public class RootController {
     public String index(Model model) {
         model.addAttribute("products", productService.getAll());
         return "index";
+    }
+
+    @GetMapping("/showUserInfo")
+    public String showUserInfo() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        Person person = ((PersonDetails) authentication.getPrincipal()).getPerson();
+        return null;
     }
 
 
