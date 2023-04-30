c4d3ae16417ce383c2ad12d990a571ac8f718428
Nikita <nikitamasslennikov@mail.ru>
2023-04-30 09:12:59 +0300

    ProductService fixed

--- freebie_coffee2/src/main/java/ru/test/freebie_coffee2/contollers/AdminController.java
+++ freebie_coffee2/src/main/java/ru/test/freebie_coffee2/contollers/AdminController.java
@@ -1,8 +1,13 @@
 package ru.test.freebie_coffee2.contollers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import ru.test.freebie_coffee2.models.Product;
 import ru.test.freebie_coffee2.services.ProductService;
+
+import javax.validation.Valid;
+import java.io.IOException;
 
 @Controller
 @RequestMapping("/admin")
@@ -14,5 +19,25 @@ public class AdminController extends AbstractRootController {
     @Override
     protected String getIndex() {
         return "admin/admin_index";
+    }
+
+    @GetMapping("/products")
+    public String products() {
+        return "admin/products/products_list";
+    }
+
+    @GetMapping("/products/create")
+    public String createProductForm(@ModelAttribute("product") Product product) {
+        return "admin/products/create";
+    }
+
+    @PostMapping("/products/create")
+    public String createProduct(@ModelAttribute("product") @Valid Product product
+            , @RequestParam("file1") MultipartFile file1
+            , @RequestParam("file2") MultipartFile file2
+            , @RequestParam("file3") MultipartFile file3) throws IOException {
+        productService.create(product, file1, file2, file3);
+        return "redirect:/admin/products";
+
     }
 }
--- freebie_coffee2/src/main/java/ru/test/freebie_coffee2/services/ProductService.java
+++ freebie_coffee2/src/main/java/ru/test/freebie_coffee2/services/ProductService.java
@@ -3,10 +3,13 @@ package ru.test.freebie_coffee2.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.Assert;
+import org.springframework.web.multipart.MultipartFile;
+import ru.test.freebie_coffee2.models.Image;
 import ru.test.freebie_coffee2.models.Product;
 import ru.test.freebie_coffee2.repositories.ProductRepository;
 import ru.test.freebie_coffee2.util.ValidationUtil;
 
+import java.io.IOException;
 import java.util.List;
 
 @Service
@@ -18,9 +21,36 @@ public class ProductService {
         this.repository = repository;
     }
 
-    public Product create(Product product) {
+    public Product create(Product product, MultipartFile file1, MultipartFile file2, MultipartFile file3) throws IOException {
+        Image image1;
+        Image image2;
+        Image image3;
+        if (file1.getSize() != 0) {
+            image1 = toImageEntity(file1);
+            image1.setPreviewImage(true);
+            product.addImage(image1);
+        }
+        if (file2.getSize() != 0) {
+            image2 = toImageEntity(file2);
+            product.addImage(image2);
+        }
+        if (file3.getSize() != 0) {
+            image3 = toImageEntity(file3);
+            product.addImage(image3);
+        }
         Assert.notNull(product, "product must not be null");
+        Product productFromDb = repository.save(product);
         return repository.save(product);
+    }
+
+    private Image toImageEntity(MultipartFile file) throws IOException {
+        Image image = new Image();
+        image.setName(file.getName());
+        image.setOriginalFileName(file.getOriginalFilename());
+        image.setContentType(file.getContentType());
+        image.setSize(file.getSize());
+        image.setBytes(file.getBytes());
+        return image;
     }
 
     public void update(Product product) {
